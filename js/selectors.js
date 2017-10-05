fbe10a3f219edfa7514e48eb93b79014957551b3
Cristy Lucke <cristy.lucke@gmail.com>
2017-10-05 16:53:52 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -15,7 +15,7 @@ function ninjaBabySelector() {
 
 // Write a function divSelector that does not accept any parameters. The function should use a class selector to return the two divs with the class pics.
 function divSelector() {
-  return $('div .pics')
+  return $('div.pics')
 }
 
 // Write a function firstListItem that does not accept any parameters. The function should use a first-child selector to return the first list item in the ul with the ID pic-list.
